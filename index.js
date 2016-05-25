50870668324efd84a5c97612e829f0977af31d65
Beer van der Drift <beerdrift@gmail.com>
2016-05-25 12:05:40 +0200

    Implemented Username support.

--- index.js
+++ index.js
@@ -2,14 +2,65 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var messages = [];
+
+var userDataConstructor = function(){
+	return {
+		loggedIn: false,
+		username: ""
+	};
+};
+
+var usernameTakenCheck = function(username){
+	return false;
+};
+
+var	makeMessage = function(sender, message){
+	return {
+		id: getUniqueMessageId(),
+		sender: sender,
+		message: message
+	};
+};
+
+var getUniqueMessageId = function(){
+	var id = 0;
+
+	while(messages.filter(mo => (mo.id == id)).length > 0){
+		id++;
+	};
+
+	return id;
+};
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	socket._userdata = userDataConstructor();
+	console.log("User connected");
+
+	socket.on('login', function(username){
+		if(username === ""){
+			this.emit("login_failed", "Username empty");
+		}
+
+		if(!usernameTakenCheck(username)){
+			this._userdata.loggedIn = true;
+			this._userdata.username = username;
+			this.emit("login_succes");
+		}else{
+			this.emit("login_failed", "Username already taken");
+		}
+	});
 
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		if(!socket._userdata.loggedIn) return;
+
+		var messageObj = makeMessage(this._userdata.username, msg);
+
+		io.emit('chat message', messageObj);
 	});	
 });
 
